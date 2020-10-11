be88ff5254aabe06061f9cd4c7d50cfc81ecae1d
Stefan <fanste474@gmail.com>
2020-10-11 14:38:01 +0200

    final changes to flesh out the logic and also added another output line to clarify who won the round in HTML

--- script.js
+++ script.js
@@ -3,7 +3,7 @@
 let playerScore = 0, computerScore = 0, rounds = 3; 
 
 /*This part sets up how many rounds there will be as well as 
-resets the score. Default number is BO3*/
+resets the score. Default number is BO3.*/
 
 function r3() {
     rounds = 3;
@@ -32,11 +32,11 @@ function r7() {
     return;
 }
 
-//This initially displays the score
+//This probably should just be hardcoded in HTML
 
 document.getElementById("scoreDisplay").innerHTML = (playerScore + " - " + computerScore);
 
-// random number generator to decide the computer's move
+// random number generator to return the computer's move when called
 function getCompInput(){
     var temp = Math.ceil(Math.random()*3);
     console.log(temp);
@@ -46,7 +46,7 @@ function getCompInput(){
 }
 
 
-//retturns weather the player won or lost
+//returns whether the player won or lost, takes the arguments from the main function
 
 function compare(playerInput, computerInput){
     if (playerInput === computerInput) return "tie";
@@ -61,20 +61,25 @@ function compare(playerInput, computerInput){
     else if (playerInput === "paper" && computerInput === "rock") return "win"
 }
 
+/* This is the main function of the game - It triggers on click whenever the player picks rock paper or scissors
+HTML passes an argument of input depending on which button is clicked, then this function calls the RNG function to get
+the computer input, which is used to run the compare function to determain the winner */
+
 function game(input){
     
-    if (compare(input, getCompInput()) == "win"){
+    computerInput = getCompInput();
+    if (compare(input, computerInput) == "win"){
         playerScore++;
         document.getElementById("scoreDisplay").innerHTML = (playerScore + " - " + computerScore);
-        
+        document.getElementById("roundWinner").innerHTML = ("You choose " + input + ", and the opponent choose " + computerInput + ". You win!");
     }
-    else if(compare(input, getCompInput()) == "loss"){
+    else if(compare(input, computerInput) == "loss"){
         computerScore++;
         document.getElementById("scoreDisplay").innerHTML = (playerScore + " - " + computerScore);
-        
+        document.getElementById("roundWinner").innerHTML = ("You choose " + input + ", and the opponent choose " + computerInput + ". You lose.");
     }
-    else if(compare(input, getCompInput()) == "tie"){
-        
+    else if(compare(input, computerInput) == "tie"){
+        document.getElementById("roundWinner").innerHTML = ("Both you and computer choose " + input + ", it's a tie!");
         return;
     }
 
